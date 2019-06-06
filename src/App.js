f3507c8bc807552623ce0d2b115bd9b7a19e5103
clarabrandt <mariaclarabrandt@gmail.com>
2019-06-06 16:01:27 +0200

    creating first components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Layout from "./components/Layout.js";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Layout></Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import "./Layout.css";
+import Banner from './Banner'
+import Content from './Content'
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        <Banner />
+        <Content />
+      </div>
+    )
+  }
+}
+
+
+export default Layout
