8d9f722e5e5bdd081040b0387b503635134fad74
clarabrandt <mariaclarabrandt@gmail.com>
2019-06-19 15:14:57 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout.js";
+import Madrinhas from "./components/Madrinhas.js";
 
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Layout></Layout>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/home" component={Layout} />
+            <Route path="/madrinhas" component={Madrinhas} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
