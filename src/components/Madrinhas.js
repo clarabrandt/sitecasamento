e068ecc16330cc84103c357fd224e4b3943da8e2
clarabrandt <mariaclarabrandt@gmail.com>
2019-06-19 15:14:35 +0200

    title and basic layout

---
+++ src/components/Madrinhas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Madrinhas.css'
+
+class Madrinhas extends Component {
+  render() {
+    return (
+      <div className='madrinhas'>
+        <div className='madrinhas-banner'>
+          <div className='madrinhas-banner--text'>As madrinhas</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Madrinhas
