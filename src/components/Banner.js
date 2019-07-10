f72ec5ab4f9c4f3c2086194856692065089fb88e
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-10 08:37:10 +0200

    banner first layout and pics

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import "./Banner.css"
+import love from './images/love.svg'
 
 export default class Banner extends Component {
   render() {
     return (
       <div className='banner'>
         <div className='banner-text'>Sá e Tomás</div>
+        <img className='heart-icon' src={love} width="40" height="40" alt="Heart" />
+
       </div>
     )
   }
