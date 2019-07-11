66a7e1897d1ec594072dc160f580c41e9b693475
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-11 18:13:40 +0200

    changing layout

--- src/components/Local.js
+++ src/components/Local.js
@@ -1,88 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './Local.css'
-import igreja from './images/igreja.jpg'
-import festa from './images/Festa.jpg'
-import love from './images/love.svg'
+// import love from './images/love.svg'
 import rings from './images/rings.svg'
 import glasses from './images/glasses.svg'
+import igreja from './images/igreja.jpg'
+import festa from './images/festa.jpg'
 
 
-class Local extends Component {
+
+export default class Local extends Component {
   render() {
     return (
       <div className='local'>
-        <div className='local-banner'>
-          <div className='local-banner--text'> O grande dia</div>
-        </div>
         <div className='local-content'>
-          <div className='local-content--banner'>
-            <div className='local-content--title'>
-              <div className='local-content--title--text'>A CERIMÔNIA</div>
-              <div className='local-content--title--icon'><img src={rings} alt="Heart" /></div>
-            </div>
-            <div className='local-content--details'>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Data: </div>
-                <div className='local-content--details--info'> 04 de janeiro de 2020</div>
-              </div>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Hora:</div>
-                <div className='local-content--details--info'>19:00 horas</div>
-              </div>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Local: </div>
-                <div className='local-content--details--info'>Paróquina Santa Margarida Maria</div>
-              </div>
-            </div>
-          </div>
           <div className='church'>
-            <div className='church-pic'>
-              <img className='church-pic--img' src={igreja} height='500' width='600' alt="Church" />
-            </div>
-            <div className='church-text'>
-              <div className='church-text--details'>
+            <div className='local-banner'>
+              <div className='local-banner--title'>
+                <div className='local-banner--title-text'>A CERIMÔNIA</div>
+                <div className='local-banner--title-icon'><img src={rings} alt="Heart" /></div>
               </div>
             </div>
-          </div>
-          <div className='icons-line'>
-            <div className='local-content--title--icon'><img src={love} alt="Heart" /></div>
-            <div className='local-content--title--icon'><img src={love} alt="Heart" /></div>
-            <div className='local-content--title--icon'><img src={love} alt="Heart" /></div>
-          </div>
-          <div className='local-content--banner'>
-            <div className='local-content--title'>
-              <div className='local-content--title--text'>A FESTA</div>
-              <div className='local-content--title--icon'><img src={glasses} alt="Heart" /></div>
+            <div className='details'>
+              <div className='local-details--block'>
+                <div className='local-details--title'>DATA: </div>
+                <div className='local-details--info'> 04/01/2020</div>
+              </div>
+              <div className='local-details--block'>
+                <div className='local-details--title'>HORA: </div>
+                <div className='local-details--info'> 19 horas</div>
+              </div>
+              <div className='local-details--block'>
+                <div className='local-details--title'>LOCAL: </div>
+                <div className='local-details--info'> Paróquina Santa Margarida Maria</div>
+              </div>
             </div>
-            <div className='local-content--details'>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Data: </div>
-                <div className='local-content--details--info'> 04 de janeiro de 2020</div>
-              </div>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Hora:</div>
-                <div className='local-content--details--info'>21:00 horas</div>
-              </div>
-              <div className='local-content--details--block'>
-                <div className='local-content--details--title'>Local: </div>
-                <div className='local-content--details--info'>Recanto das Palmeiras</div>
-              </div>
+            <div className='pic'>
+              <img className='pic--img' src={igreja} height='200' width='200' alt="Church" />
             </div>
           </div>
           <div className='party'>
-            <div className='party-pic'>
-              <img className='party-pic--img' src={festa} height='500' width='600' alt="party" />
-            </div>
-            <div className='party-text'>
-              <div className='party-text--details'>
+          <div className='local-banner'>
+              <div className='local-banner--title'>
+                <div className='local-banner--title-text'>A FESTA</div>
+                <div className='local-banner--title-icon'><img src={glasses} alt="Heart" /></div>
               </div>
             </div>
+            <div className='details'>
+              <div className='local-details--block'>
+                <div className='local-details--title'>DATA:</div>
+                <div className='local-details--info'>04 de janeiro de 2020</div>
+              </div>
+              <div className='local-details--block'>
+                <div className='local-details--title'>HORA:</div>
+                <div className='local-details--info'>21 horas</div>
+              </div>
+              <div className='local-details--block'>
+                <div className='local-details--title'>LOCAL:</div>
+                <div className='local-details--info'>RECANTO DAS PALMEIRAS</div>
+              </div>
+            </div>
+            <div className='pic'>
+              <img className='pic--img' src={festa} height='200' width='200' alt="Party" />
+            </div>
+          </div>
+          <div className='map'>
+          <img className='pic--img' src={festa} height='400' width='400' alt="Church" />
           </div>
         </div>
       </div>
-
     )
   }
 }
-
-export default Local
