1f79c38640f990957a6e53d94c973863d1d90460
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-22 18:00:34 +0200

    timeline

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,12 +4,14 @@ import Menu from './Menu'
 import Local from './Local'
 import Gallery from './Gallery'
 import RSVP from './RSVP'
+import Story2 from './Story2'
 
 export default class Content extends Component {
   render() {
     return (
       <div className='content'>
         <Menu />
+        <Story2 />
         <Local />
         <Gallery />
         <RSVP />
