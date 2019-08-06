eb7ab1972a78083209e48ea36a5c012e8c2e5a2f
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-06 10:05:26 +0200

    layout changes

--- src/components/Madrinhas.js
+++ src/components/Madrinhas.js
@@ -3,68 +3,98 @@ import './Madrinhas.css'
 import clara from './photos/eu.jpg'
 import bianca from './photos/bianca.jpg'
 import larise from './photos/larise.jpg'
-import fe from './photos/fe.jpg'
 import filipa from './photos/filipa.jpg'
+// import fe from './photos/fe.jpg'
+import fernanda from './photos/fernanda.jpeg'
+import victor from './photos/victor.jpg'
+import vilaca from './photos/vilaca.jpg'
+import ashish from './photos/ashish.jpg'
+// import antonio from './photos/antonio.jpg'
+import amanda from './photos/amanda.jpg'
+import antonio2 from './photos/Antonio2.JPG'
+import joao from './photos/joao.JPG'
+import theo from './photos/theo.JPG'
+import goncalo from './photos/goncalo.png'
 
 class Madrinhas extends Component {
   render() {
     return (
       <div className='madrinhas'>
-        <div className='madrinhas-title'>As madrinhas</div>
+        <div className='madrinhas-title'>Pajem, Madrinhas, Padrinhos</div>
         <div className='madrinhas-content'>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={clara} alt="clara" />
-            <div className='madrinhas-name'>Clara Brandt</div>
+            <img className='madrinhas-photo' src={theo} alt="theo" />
+            <div className='madrinhas-name'>Theo</div>
           </div>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={bianca} alt="bianca" />
-            <div className='madrinhas-name'>Bianca Delarue</div>
+            <img className='madrinhas-photo' src={theo} alt="amanda" />
+            <div className='madrinhas-name'>Theo</div>
           </div>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={larise} alt="larise" />
-            <div className='madrinhas-name'>Larise Barros</div>
-          </div>
-          <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={fe} alt="fernanda" />
-            <div className='madrinhas-name'>Fernanda Brandt</div>
-          </div>
-          <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={filipa} alt="filipa" />
-            <div className='madrinhas-name'>Filipa Eiró</div>
-          </div>
-          <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={larise} alt="clara" />
-            <div className='madrinhas-name'>Larise Barros</div>
+            <img className='madrinhas-photo' src={theo} alt="antonio" />
+            <div className='madrinhas-name'>Theo</div>
           </div>
         </div>
-        <div className='madrinhas-title'>Os padrinhos</div>
+        <div div className='padrinhos-content'>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={amanda} alt="amanda" />
+            <div className='madrinhas-name'>Amanda</div>
+          </div>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={antonio2} alt="antonio" />
+            <div className='madrinhas-name'>António</div>
+          </div>
+        </div>
         <div className='madrinhas-content'>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={clara} alt="clara" />
-            <div className='madrinhas-name'>Clara Brandt</div>
-          </div>
-          <div className='madrinhas-each'>
             <img className='madrinhas-photo' src={bianca} alt="bianca" />
-            <div className='madrinhas-name'>Bianca Delarue</div>
+            <div className='madrinhas-name'>Bianca</div>
           </div>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={larise} alt="larise" />
-            <div className='madrinhas-name'>Larise Barros</div>
+            <img className='madrinhas-photo' src={ashish} alt="ashish" />
+            <div className='madrinhas-name'>Ashish</div>
           </div>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={fe} alt="fernanda" />
+            <img className='madrinhas-photo' src={clara} alt="amanda" />
+            <div className='madrinhas-name'>Clara</div>
+          </div>
+        </div>
+        <div className='padrinhos-content'>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={filipa} alt="victor" />
+            <div className='madrinhas-name'>Filipa</div>
+          </div>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={joao} alt="bianca" />
+            <div className='madrinhas-name'>João</div>
+          </div>
+        </div>
+        <div className='madrinhas-content'>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={goncalo} alt="gonçalo" />
+            <div className='madrinhas-name'>Gonçalo</div>
+          </div>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={larise} alt="antonio" />
+            <div className='madrinhas-name'>Larise</div>
+          </div>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={vilaca} alt="clara" />
+            <div className='madrinhas-name'>Vilaça</div>
+          </div>
+        </div>
+        <div className='padrinhos-content'>
+          <div className='madrinhas-each'>
+            <img className='madrinhas-photo' src={fernanda} alt="fernanda" />
             <div className='madrinhas-name'>Fernanda Brandt</div>
           </div>
           <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={filipa} alt="filipa" />
-            <div className='madrinhas-name'>Filipa Eiró</div>
-          </div>
-          <div className='madrinhas-each'>
-            <img className='madrinhas-photo' src={larise} alt="clara" />
-            <div className='madrinhas-name'>Larise Barros</div>
+            <img className='madrinhas-photo' src={victor} alt="victor" />
+            <div className='madrinhas-name'>Victor Avelar</div>
           </div>
         </div>
       </div>
+
     )
   }
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import './Timeline.css'
 import festa from './images/festa.jpg'
+import alianca from './photos/alianca.jpg'
+import alianca2 from './photos/alianca2.jpg'
+import civil from './photos/civil.jpg'
+import civil2 from './photos/civil2.jpg'
+import deserto from './photos/deserto.jpg'
 import Slide from 'react-reveal/Slide';
 
 
@@ -15,7 +20,7 @@ export default class Timeline extends Component {
           <Slide left>
             <div className="history-content">
               <div className='timeline-date date-left'>04 Jan 2017</div>
-              <img className='pic--img pic-story' src={festa} height='300' width='433' alt="Party" />
+              <img className='pic--img pic-story' src={festa} width='300' height='433' alt="Party" />
               <div className='timeline-text'>Começo do ano, balada com os amigos. Nos conhecemos na Pensão do amor.</div>
             </div>
           </Slide>
@@ -51,7 +56,7 @@ export default class Timeline extends Component {
           <Slide left>
             <div className="history-content">
               <div className='timeline-date date-left'>Julho 2018</div>
-              <img className='pic--img pic-story' src={festa} height='300' width='433' alt="Party" />
+              <img className='pic--img pic-story' src={alianca} width='300' height='433' alt="Party" />
               <div className='timeline-text'>Com a proposta de irmos morar em Dubai veio também o pedido de casamento!</div>
             </div>
           </Slide>
@@ -60,7 +65,7 @@ export default class Timeline extends Component {
           <Slide right>
             <div className="history-content">
               <div className='timeline-date'>21 Set 2018</div>
-              <img className='pic--img pic-story' src={festa} height='300' width='433' alt="Party" />
+              <img className='pic--img pic-story' src={civil} width='300' height='433' alt="Party" />
               <div className='timeline-text'>Nosso casamento civil foi em Lisboa, e a família da Sá veio do Brasil para celebrar com a gente. Tivemos uma recepção depois, mas ficou decidido que a festa seria no Rio!</div>
             </div>
           </Slide>
@@ -69,7 +74,7 @@ export default class Timeline extends Component {
           <Slide left>
             <div className="history-content">
               <div className='timeline-date date-left'> 01 Outubro 2018</div>
-              <img className='pic--img pic-story' src={festa} height='300' width='433' alt="Party" />
+              <img className='pic--img pic-story' src={deserto} width='300' height='433' alt="Party" />
               <div className='timeline-text'>Nos mudamos para Dubai. Durou pouco, apenas dois meses, mas foi uma experiência engrandecedora. Pudemos viajar por ali e conhecer um pouco mais sobre a cultura local quando a família do Tomás foi nos visitar.</div>
             </div>
           </Slide>
