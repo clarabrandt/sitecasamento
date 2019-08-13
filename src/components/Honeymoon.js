ba1f72776a378553f6b8c67bd8f0cf29689a2480
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-13 10:32:12 +0200

    honeymoon cards

--- src/components/Honeymoon.js
+++ src/components/Honeymoon.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react'
+import Card from './honeymoon/Card'
 import './Honeymoon.css'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import safari from './photos/safari.jpg'
+import safari from './photos/honeymoon/safari.jpg'
 // import africa1 from './photos/africa1.jpg'
 // import africa2 from './photos/africa2.jpg'
-import africa3 from './photos/africa3.jpg'
-import hotel1 from './photos/hotel1.jpeg'
-import plain from './photos/plain.jpeg'
-import plain1 from './photos/plain1.jpeg'
-import jantar1 from './photos/jantar1.jpg'
-import bungee from './photos/bungee.jpg'
+import africa3 from './photos/honeymoon/africa3.jpg'
+import hotel1 from './photos/honeymoon/hotel1.jpeg'
+import plane from './photos/honeymoon/plane.jpeg'
+import plane1 from './photos/honeymoon/plane1.jpeg'
+import jantar1 from './photos/honeymoon/jantar1.jpg'
+import bungee from './photos/honeymoon/bungee.jpg'
 
 export default class Honeymoon extends Component {
   styles = theme => ({
     button: {
       margin: theme.spacing.unit,
+      color: 'green',
     },
     input: {
       display: 'none',
     },
   });
   render() {
-    const { classes } = this.props;
     return (
       <div className='honeymoon'>
         <img className='honeymoon-banner' src={africa3} alt='safari' />
@@ -31,172 +32,15 @@ export default class Honeymoon extends Component {
           <div className='component-title'>Presentes</div>
           <div className='honeymoon-text'>Como forma de nos presentear, além da presença de vocês, é possível colaborar com nossa lua-de-mel, que acontecerá futuramente na África do Sul!</div>
           <div className='honeymoon-cards'>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={plain1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Passagens aéreas</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>€ 120</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>20 quotas de</div>
-                  <div className='price-value'>R$ 300</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={hotel1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Diária de hotel em Joanesburgo</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>3 quotas de</div>
-                  <div className='price-value'>€ 50</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>5 quotas de</div>
-                  <div className='price-value'>R$ 150</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={safari} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Safari no Kruger Park</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>4 quotas de</div>
-                  <div className='price-value'>€ 40</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 80</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-          </div>
-          <div className='honeymoon-cards'>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={jantar1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Jantar na savana para 2</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>5 quotas de</div>
-                  <div className='price-value'>€ 40</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 100</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={hotel1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Degustação de vinhos</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'> 3 quotas de</div>
-                  <div className='price-value'>€ 50</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 75</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={bungee} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Bungee Jump na Bloukrans Bridge</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>5 quotas de</div>
-                  <div className='price-value'>€ 40</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 100</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-          </div>
-          <div className='honeymoon-cards'>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={jantar1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Jantar na savana para 2</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>5 quotas de</div>
-                  <div className='price-value'>€ 40</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 100</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={hotel1} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Diária de hotel em Joanesburgo</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>3 quotas de</div>
-                  <div className='price-value'>€ 50</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>5 quotas de</div>
-                  <div className='price-value'>R$ 150</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
-            <div className='honeymoon-card'>
-              <img className='honeymoon-card--pic' src={safari} alt='safari' width='300px' height='250px' />
-              <div className='honeymoon-card--title'>Safari no Kruger Park</div>
-              <div className='honeymoon-card--price'>
-                <div className='price-euro'>
-                  <div className='price-text'>4 quotas de</div>
-                  <div className='price-value'>€ 40</div>
-                </div>
-                <div className='price-real'>
-                  <div className='price-text'>10 quotas de</div>
-                  <div className='price-value'>R$ 80</div>
-                </div>
-              </div>
-              <div className='honeymoon-card--button'>
-                <button className='button-euro'>comprar</button>
-                <button className='button-real'>comprar</button>
-              </div>
-            </div>
+            <Card title="Passagens aéreas" photo={plane1} number={10} numero={20} euro={120} real={300} />
+            <Card title="Diária de hotel em Joanesburgo" photo={hotel1} number={3} numero={5} euro={50} real={150} />
+            <Card title="Safari no Kruger Park" photo={safari} number={4} numero={10} euro={40} real={80} />
+            <Card title="Jantar na savana para 2" photo={jantar1} number={5} numero={10} euro={40} real={100} />
+            <Card title="Degustação de vinhos" photo={hotel1} number={3} numero={10} euro={50} real={75} />
+            <Card title="Bungee Jump na Bloukrans Bridge" photo={bungee} number={5} numero={10} euro={40} real={100} />
+            <Card title="Jantar na savana para 2" photo={jantar1} number={3} numero={10} euro={40} real={100} />
+            <Card title="Diária de hotel em Joanesburgo" photo={hotel1} number={5} numero={5} euro={50} real={150} />
+            <Card title="Safari no Kruger Park" photo={safari} number={4} numero={10} euro={40} real={80} />
           </div>
         </div>
       </div>
