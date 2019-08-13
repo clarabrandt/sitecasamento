7284d468743f9be328f72cde88e25ed17ebfbfb1
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-13 10:31:54 +0200

    novo layout couple

--- src/components/Couple.js
+++ src/components/Couple.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
 import './Couple.css'
-import sa from './photos/Sa.jpg'
-import tomas from './photos/Tomas.jpg'
-import love from './images/love.svg'
+import sa from './photos/casal/Sa.jpg'
+import tomas2 from './photos/casal/Tomas2.jpg'
+
 
 export default class Couple extends Component {
   render() {
     return (
       <div className='couple'>
         <div className='component-title'>
-          <div>Nossa</div>
-          <img className='heart-icon--couple' src={love} width="60" height="50" alt="Heart" />
-          <div>história</div>
+          <div>Nossa história</div>
         </div>
         <div className='couple-content'>
           <div className='couple-details'>
@@ -20,7 +18,7 @@ export default class Couple extends Component {
           </div>
           <div className='couple-text'>Sá, carioca, menina da praia. Resolveu que queria mudar um pouco os ares e, em 2016, foi fazer uma pós em Lisboa. Uma não, duas! Não sabia que, outra vez, sua vida mudaria por completo depois do reveillon de 2017. Tomás e Sá se conheceram, e a vida foi só emoção desde então!</div>
           <div className='couple-details'>
-            <img className='couple-img' src={tomas} alt="couple" />
+            <img className='couple-img' src={tomas2} alt="couple" />
             <div className='couple-name'>Tomás Eiró</div>
           </div>
         </div>
