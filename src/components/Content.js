ec04fc28fd2c362e82a9050857aa5ca06cfa4aab
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-16 08:47:19 +0200

    responsiveness content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import "./Content.css"
-import Menu from './Menu'
 import Local from './Local'
 import Gallery from './Gallery'
 import RSVP from './RSVP'
@@ -8,7 +7,7 @@ import Couple from './Couple'
 import Timeline from './Timeline'
 import Counter from './Counter'
 import Madrinhas from './Madrinhas'
-import Honeymoon from './Honeymoon'
+import Honeymoon from './honeymoon/Honeymoon'
 import Banner2 from './Banner2'
 
 export default class Content extends Component {
@@ -16,7 +15,6 @@ export default class Content extends Component {
   render() {
     return (
       <div className='content'>
-        <Menu />
         <Couple />
         <Timeline />
         <Counter />
