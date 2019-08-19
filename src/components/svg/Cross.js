e45da324c80bb88166adddfd750fd13bd0d23683
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-19 22:40:58 +0200

    svgs

---
+++ src/components/svg/Cross.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const cross = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" version="1.1" width="40px" height="40px">
+    <g id="surface1" fill="#444649" >
+      <path d="M 15.125 12.28125 L 12.28125 15.125 L 22.21875 25 L 12.28125 34.875 L 15.125 37.71875 L 25.0625 27.84375 L 35 37.71875 L 37.8125 34.875 L 27.90625 25 L 37.8125 15.125 L 35 12.28125 L 25.0625 22.15625 Z " />
+    </g>
+  </svg>
+)
+
+export default cross;
