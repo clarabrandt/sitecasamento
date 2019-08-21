dce8b1c6af96b542043469a07339a4e0e910629a
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-21 12:05:34 +0200

    menu sticky on scroll

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,28 +7,52 @@ import love from './svg/love.svg'
 export default class Menu extends Component {
   constructor(props) {
     super(props)
-    // this.menu = React.createRef();
+    this.menu = React.createRef();
     this.state = {
-      sticky: true
+      sticky: false
     }
+    this.onScroll = this.onScroll.bind(this);
   }
- 
+
+  onScroll() {
+
+    const isTop =
+      window.pageYOffset <= 790 ? false : true;
+    this.setState({
+      sticky: isTop
+    });
+
+    console.log(window.pageYOffset)
+  }
+
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+    this.setState({
+      clicked: this.props.path
+    });
+  }
+
   render() {
     return (
-      <div className={`menu ${this.props.sticky ? 'sticky' : 'notSticky'}`} >
-        <MenuItem name='Home' anchor='home' handleClick={this.props.handleClick}/>
+      <div className={`menu ${this.state.sticky ? 'sticky' : 'notSticky'}`} ref={this.menu}>
+        <MenuItem name='Home' anchor='home' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Sobre nós' anchor='casal'handleClick={this.props.handleClick} />
+        <MenuItem name='Sobre nós' anchor='casal' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Onde e quando' anchor='local' handleClick={this.props.handleClick}/>
+        <MenuItem name='Onde e quando' anchor='local' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Galeria' anchor='galeria' handleClick={this.props.handleClick}/>
+        <MenuItem name='Galeria' anchor='galeria' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Madrinhas/Padrinhos' anchor='madrinhas' handleClick={this.props.handleClick}/>
+        <MenuItem name='Madrinhas/Padrinhos' anchor='madrinhas' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Presentes' anchor='honeymoon' handleClick={this.props.handleClick}/>
+        <MenuItem name='Presentes' anchor='honeymoon' handleClick={this.props.handleClick} />
         <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='RSVP' anchor='rsvp' handleClick={this.props.handleClick}/>
+        <MenuItem name='RSVP' anchor='rsvp' handleClick={this.props.handleClick} />
       </div>
     )
   }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
 import "./MenuItem.css";
 
 export default class MenuItem extends Component {
   render() {
     return (
       <div className="menu--item--style">
-        <NavLink
+        <div
           style={{ textDecoration: "none" }}
           className="menu--item"
           to={this.props.anchor}
           onClick={() => this.props.handleClick(this.props.anchor)}
         >
           <div className="menu-item--name">{this.props.name}</div>
-        </NavLink>
+        </div>
       </div>
     );
   }
