ea30e52b9bce7c1842ee0baec882f70b69c0699a
clarabrandt <mariaclarabrandt@gmail.com>
2019-08-21 18:40:53 +0200

    responsive menu for small screens

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 import './Menu.css';
 import MenuItem from './MenuItem';
 import love from './svg/love.svg'
+import Cross from './svg/Cross.js'
+import Hamburger from './svg/Hamburger.js'
 
 export default class Menu extends Component {
   constructor(props) {
     super(props)
     this.menu = React.createRef();
     this.state = {
-      sticky: false
+      sticky: false,
+      open: false
     }
     this.onScroll = this.onScroll.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
   }
 
   onScroll() {
@@ -21,8 +25,6 @@ export default class Menu extends Component {
     this.setState({
       sticky: isTop
     });
-
-    console.log(window.pageYOffset)
   }
 
 
@@ -37,23 +39,51 @@ export default class Menu extends Component {
     });
   }
 
+  toggleMenu() {
+    if (this.state.open === false) {
+      this.setState({
+        open: true
+      })
+    } else {
+      this.setState({
+        open: false
+      })
+    }
+  }
+
   render() {
     return (
       <div className={`menu ${this.state.sticky ? 'sticky' : 'notSticky'}`} ref={this.menu}>
-        <MenuItem name='Home' anchor='home' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Sobre nós' anchor='casal' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Onde e quando' anchor='local' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Galeria' anchor='galeria' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Madrinhas/Padrinhos' anchor='madrinhas' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='Presentes' anchor='honeymoon' handleClick={this.props.handleClick} />
-        <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
-        <MenuItem name='RSVP' anchor='rsvp' handleClick={this.props.handleClick} />
+          <MenuItem name='Home' anchor='home' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='Sobre nós' anchor='casal' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='Onde e quando' anchor='local' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='Galeria' anchor='galeria' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='Madrinhas/Padrinhos' anchor='madrinhas' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='Presentes' anchor='honeymoon' handleClick={this.props.handleClick} />
+          <img className='heart-icon--menu' src={love} width="8" height="8" alt="Heart" />
+          <MenuItem name='RSVP' anchor='rsvp' handleClick={this.props.handleClick} />
+        <div className={`hamburger ${this.state.open ? 'open' : 'closed'}`} onClick={this.toggleMenu}>
+          <Hamburger/>
+        </div>
+        <div className={`cross ${this.state.open ? 'open' : 'closed'}`} onClick={this.toggleMenu}>
+          <Cross />
+        </div>
+        <div className={`menu--dropdown ${this.state.open ? 'open' : 'closed'}`} onClick={this.toggleMenu} >
+          <MenuItem name='Home' anchor='home' handleClick={this.props.handleClick} />
+          <MenuItem name='Sobre nós' anchor='casal' handleClick={this.props.handleClick} />
+          <MenuItem name='Onde e quando' anchor='local' handleClick={this.props.handleClick} />
+          <MenuItem name='Galeria' anchor='galeria' handleClick={this.props.handleClick} />
+          <MenuItem name='Madrinhas/Padrinhos' anchor='madrinhas' handleClick={this.props.handleClick} />
+          <MenuItem name='Presentes' anchor='honeymoon' handleClick={this.props.handleClick} />
+          <MenuItem name='RSVP' anchor='rsvp' handleClick={this.props.handleClick} />
       </div>
+      </div>
+      
     )
   }
 }
